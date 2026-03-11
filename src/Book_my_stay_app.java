2c1a8fb025945d3c847d37dbb36a7ea35c06cd0c
Rithu <rp0772@srmist.edu.in>
2026-03-11 09:33:05 +0530

    Add use case 2

--- src/Book_my_stay_app.java
+++ src/Book_my_stay_app.java
@@ -1,9 +1,28 @@
-
 public class Book_my_stay_app {
-
     public static void main(String[] args) {
+        System.out.println("Hotel Room Initialization\n");
 
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
+        // Initialize the objects
+        Room single = new SingleRoom();
+        Room doubleRm = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        // Static availability variables (as requested in Use Case 2)
+        int singleAvail = 5;
+        int doubleAvail = 3;
+        int suiteAvail = 2;
+
+        // Display results
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvail + "\n");
+
+        System.out.println("Double Room:");
+        doubleRm.displayRoomDetails();
+        System.out.println("Available: " + doubleAvail + "\n");
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvail);
     }
 }
