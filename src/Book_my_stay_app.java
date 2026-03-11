18bb3931f65f8ed4c0b9d58943ca65b558a2349e
Rithu <rp0772@srmist.edu.in>
2026-03-11 10:10:45 +0530

    Cleanup master: Only ReadMe remains

--- src/Book_my_stay_app.java
+++
@@ -1,8 +0,0 @@
-public class Book_my_stay_app {
-
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
-    }
-}
